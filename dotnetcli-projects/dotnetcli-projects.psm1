c1d7dfe01a6cde0559103b3666cc1cd1fdcb265e
ebl <contact@ebl-consulting.com>
2019-06-23 17:29:41 +0200

    remove duplicated templates in _init folder
    update PS module to integrate previous initialize.ps1 script

--- dotnetcli-projects/dotnetcli-projects.psm1
+++ dotnetcli-projects/dotnetcli-projects.psm1
@@ -3,46 +3,6 @@ Set-StrictMode -Version Latest
 $sourceFolder = "src"
 $testFolder = "test"
 
-function Create-Projects
- {
-    Param(
-        # indicate the root namespace for projects
-        [Parameter(HelpMessage="root namespace")] [string] $RootNamespace
-        # indicate reference(s) for core project
-        ,[Parameter(HelpMessage="core project references")] [string[]] $CorePackages = @()
-        # indicate reference(s) for test project
-        ,[Parameter(HelpMessage="test project references")] [string[]] $TestPackages = @()
-    )
- 
-    if (!$RootNamespace) {
-        $RootNamespace = [System.IO.Path]::GetFileName((Get-Location).Path)
-    }
-    
-    # # create core project
-    # Write-Verbose "creating core project: ${RootNamespace}.core..."
-    # dotnet new zericcolib --name "${RootNamespace}.core" -o "${sourceFolder}\${RootNamespace}.core\" --no-restore
-    # foreach ($reference in $CorePackages) {
-    #     Write-Debug "adding core project reference: $reference"
-    #     dotnet add ".\${sourceFolder}\${RootNamespace}.core\${RootNamespace}.core.csproj" package $CorePackages
-    # }
-
-    # # create test project
-    # Write-Verbose "creating test project: ${RootNamespace}.tests..."
-    # dotnet new zericcout --name "${RootNamespace}.tests" -o "${testFolder}\${RootNamespace}.tests\" --no-restore
-    # foreach ($reference in $TestPackages) {
-    #     Write-Debug "adding test project reference: $reference"
-    #     dotnet add ".\${testFolder}\${RootNamespace}.tests\${RootNamespace}.tests.csproj" package $reference
-    # }
-
-    dotnet new zericcosol
-
-    # create solution
-    dotnet new sln
-    foreach ($csproj in (Get-ChildItem -Recurse -Path "." -Filter "*.csproj")) {
-        dotnet sln add $csproj.FullName
-    }
-}
-
 function New-ConsoleProject
 {
     Param(
@@ -70,7 +30,7 @@ function New-ConsoleProject
     dotnet sln add ".\${sourceFolder}\${name}\${name}.csproj"
 }
 
-function Apply-Template([string] $template) {
+function Use-Template([string] $template) {
     $targetPath = $template -replace ".tpl.ps1", ""
     if (Test-Path $targetPath) {
         Write-Information "$targetPath already exists, skipping"
@@ -87,15 +47,80 @@ function Apply-Template([string] $template) {
     . "$($template.FullName)" > $targetPath
 }
 
-function Apply-Templates([System.IO.DirectoryInfo] $sourceFolder) {
+function Use-Templates([System.IO.DirectoryInfo] $sourceFolder) {
     Write-Information "applying templates from folder ${sourceFolder}..."
     foreach ($template in Get-ChildItem -Path $sourceFolder.FullName -Filter "*.tpl.ps1") {
-        Apply-Template $template.Name
+        Use-Template $template.Name
     }
     foreach ($subFolder in Get-ChildItem -Path $sourceFolder.FullName -Directory) {
         Write-Information "applying templates from folder ${sourceFolder}\${subFolder}..."
         foreach ($template in Get-ChildItem -Path $subFolder.FullName -Filter "*.tpl.ps1") {
-            Apply-Template (Join-Path $subFolder.Name $template.Name)
+            Use-Template (Join-Path $subFolder.Name $template.Name)
         }    
     }
+}
+
+function New-Solution
+{
+    [CmdletBinding()]
+    Param(
+        # indicate the root namespace for projects
+        [Parameter(HelpMessage="root namespace")] [string] $RootNamespace
+        # indicate reference(s) for core project
+        ,[Parameter(HelpMessage="core project references")] [string[]] $CorePackages
+        # indicate reference(s) for test project
+        ,[Parameter(HelpMessage="test project references")] [string[]] $TestPackages = @()
+    )
+
+    if (!$RootNamespace) {
+        $RootNamespace = [System.IO.Path]::GetFileName((Get-Location).Path)
+    }
+
+    Set-StrictMode -Version Latest
+
+    if (!$RootNamespace) {
+        $RootNamespace = [System.IO.Path]::GetFileName((Get-Location).Path)
+    }
+    
+    # # create core project
+    # Write-Verbose "creating core project: ${RootNamespace}.core..."
+    # dotnet new zericcolib --name "${RootNamespace}.core" -o "${sourceFolder}\${RootNamespace}.core\" --no-restore
+    # foreach ($reference in $CorePackages) {
+    #     Write-Debug "adding core project reference: $reference"
+    #     dotnet add ".\${sourceFolder}\${RootNamespace}.core\${RootNamespace}.core.csproj" package $CorePackages
+    # }
+
+    # # create test project
+    # Write-Verbose "creating test project: ${RootNamespace}.tests..."
+    # dotnet new zericcout --name "${RootNamespace}.tests" -o "${testFolder}\${RootNamespace}.tests\" --no-restore
+    # foreach ($reference in $TestPackages) {
+    #     Write-Debug "adding test project reference: $reference"
+    #     dotnet add ".\${testFolder}\${RootNamespace}.tests\${RootNamespace}.tests.csproj" package $reference
+    # }
+
+    if (!(Test-Path "ArtifactFolder")) {
+        $ArtifactFolder = "artifacts"
+    }
+    if (!(Test-Path "IntermediateFolder")) {
+        $IntermediateFolder = "artifacts\obj"
+    }
+    if (!(Test-Path "Author")) {
+        $Author = "EBL"
+    }
+    if (!(Test-Path "Company")) {
+        $Company = "EBL Inc."
+    }
+
+    dotnet new zericcosol --Author $Author --ArtifactFolder $ArtifactFolder --IntermediateFolder $IntermediateFolder --Company $Company
+
+    # create solution
+    dotnet new sln
+    foreach ($csproj in (Get-ChildItem -Recurse -Path "." -Filter "*.csproj")) {
+        dotnet sln add $csproj.FullName
+    }
+
+    git init > $null
+    git add .\src > $null
+    git add .\test > $null
+    git add .\ > $null
 }
